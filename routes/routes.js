3fdb12ac6b69774b5bb6d5cfe8b811a703fa7a22
Ogutierrez08 <oscar.vidasoft@gmail.com>
2019-05-31 17:35:19 -0500

    Fix #1

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,12 @@
 module.exports = (app, data) => {
   app.get('/api/dev/:query', (req, res) => {
     let test = []
-    let palabras = []
     const regex = new RegExp((req.params.query).replace(/\*/g, '.*').replace(/\?/g, '.'))
     console.log(regex)
     for (let i = 0; i < data.length; i++) {
-      palabras = data[i].name.split(' ')
-      palabras.map(element => {
-        if (regex.test(element) === true) {
-          test.push(element)
-        }
-      })
+      if (regex.test(data[i].name) === true) {
+        test.push(data[i].name)
+      }
     }
     res.json(test)
   })
