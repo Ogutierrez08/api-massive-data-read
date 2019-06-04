8dfcd968270f75baf665010750d23f02a7182d12
Ogutierrez08 <42872009+Ogutierrez08@users.noreply.github.com>
2019-06-03 22:04:31 -0500

    Optional Parameters + Top + Cs

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,78 @@
 module.exports = (app, data) => {
   app.get('/api/dev/', (req, res) => {
-    let query = req.query.query.toUpperCase()
     let test = []
-    const regex = new RegExp((query).replace(/\*/g, '.*').replace(/\?/g, '.'))
-    console.log(regex)
-    for (let i = 0; i < data.length; i++) {
-      if (regex.test(data[i].name) === true) {
-        test.push(data[i].name)
+    if (req.query.top !== undefined && req.query.cs !== undefined && req.query.query !== undefined) {
+      if (req.query.cs !== '1') {
+        const regex = new RegExp('^' + (req.query.query.toUpperCase()).replace(/\*/g, '.*').replace(/\?/g, '.') + '$')
+        console.log(regex)
+        for (let i = 0; i < data.length; i++) {
+          if (regex.test(data[i].name) === true) {
+            if (test.length <= req.query.top) {
+              test.push(data[i].name)
+            } else {
+              break
+            }
+          }
+        }
+        res.json(test)
+      } else {
+        const regex = new RegExp('^' + (req.query.query).replace(/\*/g, '.*').replace(/\?/g, '.') + '$')
+        console.log(regex)
+        for (let i = 0; i < data.length; i++) {
+          if (regex.test(data[i].name) === true) {
+            if (test.length <= req.query.top) {
+              test.push(data[i].name)
+            } else {
+              break
+            }
+          }
+        }
+        res.json(test)
       }
+    } else if (req.query.top === undefined && req.query.query !== undefined && req.query.cs !== undefined) {
+      if (req.query.cs !== '1') {
+        const regex = new RegExp('^' + (req.query.query.toUpperCase()).replace(/\*/g, '.*').replace(/\?/g, '.') + '$')
+        console.log(regex)
+        for (let i = 0; i < data.length; i++) {
+          if (regex.test(data[i].name) === true) {
+            test.push(data[i].name)
+          }
+        }
+        res.json(test)
+      } else {
+        const regex = new RegExp('^' + (req.query.query).replace(/\*/g, '.*').replace(/\?/g, '.') + '$')
+        console.log(regex)
+        for (let i = 0; i < data.length; i++) {
+          if (regex.test(data[i].name) === true) {
+            test.push(data[i].name)
+          }
+        }
+        res.json(test)
+      }
+    } else if (req.query.cs === undefined && req.query.query !== undefined && req.query.top !== undefined) {
+      const regex = new RegExp('^' + (req.query.query).replace(/\*/g, '.*').replace(/\?/g, '.') + '$')
+      console.log(regex)
+      for (let i = 0; i < data.length; i++) {
+        if (regex.test(data[i].name) === true) {
+          if (test.length <= req.query.top) {
+            test.push(data[i].name)
+          } else {
+            break
+          }
+        }
+      }
+      res.json(test)
+    } else if (req.query.query !== undefined && req.query.top === undefined && req.query.cs === undefined) {
+      const regex = new RegExp('^' + (req.query.query).replace(/\*/g, '.*').replace(/\?/g, '.') + '$')
+      console.log(regex)
+      for (let i = 0; i < data.length; i++) {
+        if (regex.test(data[i].name) === true) {
+          test.push(data[i].name)
+        }
+      }
+      res.json(test)
+    } else if (req.query.query === undefined) {
+      res.json('El parametro query es obligatorio')
     }
-    res.json(test)
   })
 }
